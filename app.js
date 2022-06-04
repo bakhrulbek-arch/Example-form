7eed5d8ca20b10805b1eb1291e068e9f7d4284a1
Bakhrulbek <arch_ghost@hotmail.com>
2022-06-05 01:31:02 +0500

    update_v.4

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ let inp = document.querySelectorAll(".inp_v")
 let need = document.querySelectorAll(".need_set")
 inp.forEach(inp_v => {
     inp_v.onfocus = () => {
-        need.forEach(item => {
-            item.classList.add("active_clr")
-        })
-        inp_v.classList.add("active_inp")
+        if (inp.value !== '') {
+            need.forEach(item => {
+                item.classList.add("deactive_clr")
+            })
+            inp_v.classList.add("deactive_inp")
+        } else {
+            need.forEach(item => {
+                item.classList.add("active_clr")
+            })
+            inp_v.classList.add("active_inp")
+        }
+
     }
 });
 
@@ -13,6 +21,32 @@ let btn_save = document.querySelector(".btn_form_save")
 btn_save.onclick = () => {
     btn_save.style.background = "red"
 }
+
+// let inp = document.querySelector(".inp_v")
+// let need = document.querySelectorAll(".need_set")
+// inp.forEach(inp_v => {
+//     inp.onkeyup = () => {
+//         inp_v.onfocus = () => {
+//             if (inp.value.lenght == true) {
+//                 need.forEach(item => {
+//                     item.classList.add("active_clr")
+//                 })
+//                 inp_v.classList.add("active_inp")
+//             } else {
+//                 need.forEach(item => {
+//                     item.classList.add("deactive_clr")
+//                 })
+//                 inp_v.classList.add(".deactive_inp")
+//             }
+//         }
+//         console.log(inp.value);
+//     }
+// });
+
+// let btn_save = document.querySelector(".btn_form_save")
+// btn_save.onclick = () => {
+//     btn_save.style.background = "red"
+// }
     // inp_v.onblur = () => {
     //     inp_v.forEach(deactive => {
     //         deactive.classList.remove("deactive")
