05b652ecd13a619ec6f9eb52439b68b22698ec57
Bakhrulbek <arch_ghost@hotmail.com>
2022-06-04 22:43:23 +0500

    Update_v.2

---
+++ app.js
@@ -0,0 +1,5 @@
+let title_obj = document.querySelectorAll(".title_obj_inp_top")
+let inp = document.querySelectorAll(".inp_v")
+let title_obj_bottom = document.querySelectorAll(".title_obj_inp_bottom")
+
+inp
