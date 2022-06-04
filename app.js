027934c84c4baba02609ead9ea781df96b2d7770
Bakhrulbek <arch_ghost@hotmail.com>
2022-06-05 00:49:38 +0500

    update_v.3

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-let title_obj = document.querySelectorAll(".title_obj_inp_top")
 let inp = document.querySelectorAll(".inp_v")
-let title_obj_bottom = document.querySelectorAll(".title_obj_inp_bottom")
+let need = document.querySelectorAll(".need_set")
+inp.forEach(inp_v => {
+    inp_v.onfocus = () => {
+        need.forEach(item => {
+            item.classList.add("active_clr")
+        })
+        inp_v.classList.add("active_inp")
+    }
+});
 
-inp
+    // inp_v.onblur = () => {
+    //     inp_v.forEach(deactive => {
+    //         deactive.classList.remove("deactive")
+    //     })
+    //     inp_v.classList.add("deactive")
+    // }
+
+// inp.forEach(inp_val => {
+//     inp_val.onblur = () => {
+//         inp.forEach(deactive => {
+//             deactive.classList.remove("active")
+//         })
+//         inp_val.classList.add("deactive")
+//     }
+// });
